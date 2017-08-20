82e43cd3efab8aa635d00595d7f2d364ddc27588
Burak Karaoglan <burakkaraoglan@icloud.com>
2017-08-20 11:03:58 +0300

    initial commit for jwt on angularjs

---
+++ app/scripts/app.config.js
@@ -0,0 +1,14 @@
+angular
+	.module('psJwtApp').config(function ($urlRouterProvider, $locationProvider, $stateProvider) {
+		$urlRouterProvider.otherwise('/');
+		$locationProvider.html5Mode(true);
+		$stateProvider.state('register', {
+			url: '/register',
+			templateUrl: 'views/register.html'
+		});
+		$stateProvider.state('main', {
+			url: '/',
+			templateUrl: 'views/main.html'
+		});
+
+	});
